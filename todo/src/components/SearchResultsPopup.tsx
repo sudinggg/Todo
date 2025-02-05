bb10745809bab0ea373dee7847f0e8d6a9fe0425
sudinggg <115580334+sudinggg@users.noreply.github.com>
2025-02-05 15:54:32 +0900

    feat. 검색 popup 완성

--- todo/src/components/SearchResultsPopup.tsx
+++ todo/src/components/SearchResultsPopup.tsx
@@ -8,15 +8,14 @@ interface SearchResultsPopupProps {
       <div className="popup-overlay">
         <div className="popup" style={{backgroundColor:'#C5DEDA'}}>
        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: "1vh", width: '100%' }}>
-  <div style={{ fontFamily:'hanb',fontSize: '25px', margin: '0 auto' }}>Search_Results</div>
-  <div style={{color:'#C5DEDA'}}><button onClick={onClose}>x</button></div>
+  <div style={{paddingLeft:'5vw', fontFamily:'hanb',fontSize: '25px', margin: '0 auto' ,height:'3vh'}}>Search_Results</div>
+  <div><button style={{ color:'black',fontSize:'21px',background: "none",border: "none",height:'1vh'}} onClick={onClose}>x</button></div>
 </div>
-
 <div className="list-container">
           {results.length > 0 ? (
             results.map((result, index) => (
               <div key={index} className="popup-month">
-                <div> {result.month}</div>
+                <div style={{fontFamily:'hanb'}}> {result.month}</div>
                 <div >   <ul>
                   {result.tasks.map((task, i) => (
                     <li key={i}>{task}</li>
@@ -31,6 +30,5 @@ interface SearchResultsPopupProps {
       </div>
     );
   };
-  
   export default SearchResultsPopup;
   
